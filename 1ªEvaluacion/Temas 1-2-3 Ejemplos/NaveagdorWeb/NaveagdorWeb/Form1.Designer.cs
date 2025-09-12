186ba175e48b866a90b761390900725c9ecbd4c9
Diego Costa <Diego Costa@DESKTOP-LM520H3>
2025-09-12 10:44:06 +0200

    Agregar archivos de proyecto.

---
+++ 1ªEvaluacion/Temas 1-2-3 Ejemplos/NaveagdorWeb/NaveagdorWeb/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace NaveagdorWeb
+{
+    partial class NavegadorWeb
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pulsar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // Pulsar
+            // 
+            this.Pulsar.Location = new System.Drawing.Point(12, 12);
+            this.Pulsar.Name = "Pulsar";
+            this.Pulsar.Size = new System.Drawing.Size(96, 35);
+            this.Pulsar.TabIndex = 0;
+            this.Pulsar.Text = "Pulsar";
+            this.Pulsar.UseVisualStyleBackColor = true;
+            this.Pulsar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 73);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(315, 275);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // NavegadorWeb
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(339, 360);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Pulsar);
+            this.Name = "NavegadorWeb";
+            this.Text = "Navegador";
+            this.Load += new System.EventHandler(this.NavegadorWeb_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Pulsar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ 1ªEvaluacion/Temas 1-2-3 Ejemplos/TemaIntroduccion/TemaIntroduccion/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace TemaIntroduccion
+{
+    internal class Program
+    {
+            //Ejemplo referencia
+            static void suma(int a, int b, ref int c)
+            {
+                c = a + b; // c se puede usar para sumarlo
+            }
+            
+            //Ejemplo Out
+            static void sumaOut(int a, int b, out int c)
+            {
+                c = a + b; //c solo puede usarse para devolver el resultado
+            }
+
+            //Ejemplo parametros con nombre y opcionales
+            static void suma10(int a, int b = 10)//Los parámetros opcionales deben ir al final de la lista de parámetros
+            {
+            Console.WriteLine(a + b);
+            }
+
+            //Ejemplo tuplas 
+            static (int, int, int) Operaciones(int a, int b)
+            {
+                return (a + b, a - b, a * b);
+            }
+    
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); // println
+            Console.Write("Hola, Mundo!"); // print
+            Console.WriteLine("Un \ttabulador\nlínea distinta");
+            Console.WriteLine(@"Un \ttabulador\nlínea distinta"); //con @ no interpreta caracteres especiales
+            /*int c = null; //error
+            int? b = null; //acepta null */
+            int? i = 0;
+            int b = i ?? -1; //si i es null, b vale -1
+            do
+            {
+                Console.WriteLine(i++);
+            } while (i < 5);
+            Console.WriteLine();
+            i = 0;
+            do
+            {
+                Console.WriteLine(++i);
+            } while (i < 5);
+
+            //Ejemplo referencia
+            int res = 0; // Obligatorio inicializar res antes de pasarlo
+            suma(12, 34, ref res);
+            Console.WriteLine("Resultado: {0}", res);
+
+            //Ejemplo Out
+            int res1; // out indica que res se inicia dentro del método
+            sumaOut(12, 34, out res1);
+            Console.WriteLine("Resultado: {0}", res1);
+
+            //Ejemplo parametros con nombre y opcionales
+            suma10(3, 4);
+            suma10(b: 4, a: 3);
+            suma10(3);
+
+            //Ejemplo tuplas 
+            var res2 = Operaciones(1, 2);
+            // En lugar de var, se puede poner (int,int,int)
+            Console.WriteLine("Sum:{0,4}\nRes:{1,4}\nMul:{2,4}",
+            res2.Item1, res2.Item2, res2.Item3);
+
+        }
+    }
+}
