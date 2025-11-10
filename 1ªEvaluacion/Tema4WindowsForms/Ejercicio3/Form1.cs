3bd714e024d3be4b7ad199fc9750324c3c2d9bfb
CojoCosta <dcosta1882000@gmail.com>
2025-11-10 09:50:55 +0100

    changes

---
+++ 1ªEvaluacion/Tema4WindowsForms/Ejercicio3/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = this.Text + " - " + 
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                checkBox1.ForeColor = Color.Red;
+            }
+            else
+            {
+                checkBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.Title = "Seleciciona una imagen";
+            of.InitialDirectory = "c:\\";
+            of.Filter = "Imagenes (*.png) | *.png | Imagenes (*.jpg) | *.jpg | Todos los archivos (*.*) | *.*";
+
+        }
+    }
+}
