e7f56aa05a2eb1a5f3e82bde29fadfae41687b10
CojoCosta <dcosta1882000@gmail.com>
2025-11-14 13:55:07 +0100

    changes 14/11

--- 1ªEvaluacion/Tema4WindowsForms/Ejercicio5/Form1.Designer.cs
+++ 1ªEvaluacion/Tema4WindowsForms/Ejercicio5/Form1.Designer.cs
@@ -28,13 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grabarNúmeroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mostrarAgendaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.separadorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btReset = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.acercaDeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grabarNúmeroToolStripMenuItem,
+            this.mostrarAgendaToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.separadorToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // grabarNúmeroToolStripMenuItem
+            // 
+            this.grabarNúmeroToolStripMenuItem.Name = "grabarNúmeroToolStripMenuItem";
+            this.grabarNúmeroToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.grabarNúmeroToolStripMenuItem.Text = "Grabar número";
+            // 
+            // mostrarAgendaToolStripMenuItem
+            // 
+            this.mostrarAgendaToolStripMenuItem.Name = "mostrarAgendaToolStripMenuItem";
+            this.mostrarAgendaToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.mostrarAgendaToolStripMenuItem.Text = "Mostrar agenda";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            // 
+            // separadorToolStripMenuItem
+            // 
+            this.separadorToolStripMenuItem.Name = "separadorToolStripMenuItem";
+            this.separadorToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.separadorToolStripMenuItem.Text = "Separador";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de...";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(245, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btReset
+            // 
+            this.btReset.Location = new System.Drawing.Point(362, 415);
+            this.btReset.Name = "btReset";
+            this.btReset.Size = new System.Drawing.Size(75, 23);
+            this.btReset.TabIndex = 2;
+            this.btReset.Text = "RESET";
+            this.btReset.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btReset);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grabarNúmeroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mostrarAgendaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem separadorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btReset;
     }
 }
 
--- 1ªEvaluacion/Tema4WindowsForms/Ejercicio5/Form1.cs
+++ 1ªEvaluacion/Tema4WindowsForms/Ejercicio5/Form1.cs
@@ -12,9 +12,72 @@ namespace Ejercicio5
 {
     public partial class Form1 : Form
     {
+        Color btoriginal;
         public Form1()
         {
             InitializeComponent();
+            btoriginal = btReset.BackColor;
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = 250;
+            int y = 100;
+            for (int i = 1; i <= 12; i++)
+            {
+                Button boton = new Button();
+                boton.Location = new Point(x, y);
+                boton.Size = new Size(40, 40);
+                boton.Enabled = true;
+                boton.Text = $"{i}";
+                if (i == 10)
+                {
+                    boton.Text = "*";
+                }
+                else if (i == 11)
+                {
+                    boton.Text = "0";
+                }
+                else if (i == 12)
+                {
+                    boton.Text = "#";
+                }
+                this.Controls.Add(boton);
+                x += 40;
+                if (i % 3 == 0)
+                {
+                    x = 250;
+                    y += 40;
+                }
+                boton.MouseDown += Boton_MouseDown;
+                boton.MouseEnter += Boton_MouseEnter;
+                boton.MouseLeave += Boton_MouseLeave;
+            }
+        }
+        private void Boton_MouseDown(object sender, MouseEventArgs e)
+        {
+            ((Button)sender).BackColor = Color.Red;
+        }
+
+        private void Boton_MouseEnter(object sender, EventArgs e)
+        {
+            if (((Button)sender).BackColor != Color.Red)
+            {
+                ((Button)sender).BackColor = Color.Orange;
+            }
+        }
+
+        private void Boton_MouseLeave(object sender, EventArgs e)
+        {
+            if (((Button)sender).BackColor != Color.Red)
+            {
+                ((Button)sender).BackColor = btoriginal;
+            }
+        }
+
+
     }
+
+
 }
+
