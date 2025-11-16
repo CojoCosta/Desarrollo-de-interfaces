37cf117476b1844e42e1b5527028786d6bb01b7f
CojoCosta <diegocostapereira18@gmail.com>
2025-11-16 18:20:47 +0100

    changes 16/11

--- 1ªEvaluacion/RepasoExamen/RepasoExamen/FrmPrincipal.Designer.cs
+++ 1ªEvaluacion/RepasoExamen/RepasoExamen/FrmPrincipal.Designer.cs
@@ -28,58 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.btnJugar = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.LstNombres = new System.Windows.Forms.ListBox();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.acionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jugarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblResultados = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnJugar
             // 
-            this.button1.Location = new System.Drawing.Point(97, 485);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 38);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "BtnJugar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnJugar.Location = new System.Drawing.Point(86, 388);
+            this.btnJugar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnJugar.Name = "btnJugar";
+            this.btnJugar.Size = new System.Drawing.Size(118, 30);
+            this.btnJugar.TabIndex = 0;
+            this.btnJugar.Text = "BtnJugar";
+            this.btnJugar.UseVisualStyleBackColor = true;
+            this.btnJugar.Click += new System.EventHandler(this.btnJugar_Click);
             // 
-            // button2
+            // btnReset
             // 
-            this.button2.Location = new System.Drawing.Point(311, 485);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(133, 38);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "BtnReset";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnReset.Location = new System.Drawing.Point(276, 388);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(118, 30);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "BtnReset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
-            // button3
+            // btnSalir
             // 
-            this.button3.Location = new System.Drawing.Point(528, 485);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(133, 38);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "BtnSalir";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnSalir.Location = new System.Drawing.Point(479, 388);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(118, 30);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "BtnSalir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // LstNombres
+            // 
+            this.LstNombres.FormattingEnabled = true;
+            this.LstNombres.ItemHeight = 16;
+            this.LstNombres.Location = new System.Drawing.Point(736, 54);
+            this.LstNombres.Name = "LstNombres";
+            this.LstNombres.Size = new System.Drawing.Size(198, 84);
+            this.LstNombres.TabIndex = 4;
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.Location = new System.Drawing.Point(780, 144);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(110, 31);
+            this.BtnEliminar.TabIndex = 5;
+            this.BtnEliminar.Text = "BtnEliminar";
+            this.BtnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acionesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 33);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // acionesToolStripMenuItem
+            // 
+            this.acionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jugarToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.salirToolStripMenuItem});
+            this.acionesToolStripMenuItem.Name = "acionesToolStripMenuItem";
+            this.acionesToolStripMenuItem.Size = new System.Drawing.Size(90, 29);
+            this.acionesToolStripMenuItem.Text = "Aciones";
+            // 
+            // jugarToolStripMenuItem
+            // 
+            this.jugarToolStripMenuItem.Name = "jugarToolStripMenuItem";
+            this.jugarToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.jugarToolStripMenuItem.Text = "Jugar";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.resetToolStripMenuItem.Text = "Reset";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(267, 6);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // lblResultados
+            // 
+            this.lblResultados.AutoSize = true;
+            this.lblResultados.Location = new System.Drawing.Point(736, 388);
+            this.lblResultados.Name = "lblResultados";
+            this.lblResultados.Size = new System.Drawing.Size(76, 16);
+            this.lblResultados.TabIndex = 7;
+            this.lblResultados.Text = "Resultados";
             // 
             // FrmPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 565);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1008, 452);
+            this.Controls.Add(this.lblResultados);
+            this.Controls.Add(this.BtnEliminar);
+            this.Controls.Add(this.LstNombres);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnJugar);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FrmPrincipal";
             this.Text = "Loteria Simple";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormPrincipal_FormClosing);
             this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnJugar;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox LstNombres;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem acionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jugarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.Label lblResultados;
     }
 }
 
--- 1ªEvaluacion/RepasoExamen/RepasoExamen/FrmPrincipal.cs
+++ 1ªEvaluacion/RepasoExamen/RepasoExamen/FrmPrincipal.cs
@@ -13,9 +13,13 @@ namespace RepasoExamen
     public partial class FrmPrincipal : Form
     {
         CheckBox ch;
+        Button[] botones;
+        ToolTip toolTip = new ToolTip();
         public FrmPrincipal()
         {
             InitializeComponent();
+            timer1.Start();
+            botones = new Button[] { btnJugar, btnReset, btnSalir };
         }
 
         private void FrmPrincipal_Load(object sender, EventArgs e)
@@ -27,8 +31,8 @@ namespace RepasoExamen
                 ch = new CheckBox();
                 ch.Enabled = true;
                 ch.Visible = true;
-                ch.Size = new Size(30,30);
-                ch.Location = new Point(x,y);
+                ch.Size = new Size(30, 30);
+                ch.Location = new Point(x, y);
                 ch.Text = i.ToString();
                 x += 40;
                 if (i % 9 == 0)
@@ -36,8 +40,81 @@ namespace RepasoExamen
                     x = 30;
                     y += 40;
                 }
+                ch.CheckedChanged += checkBox_Checked;
                 Controls.Add(ch);
+                toolTip.SetToolTip(ch, "No marcado");
             }
+        }
+        int i = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (Button boton in botones)
+            {
+                boton.BackColor = Color.Beige;
+            }
+            botones[i].BackColor = Color.Yellow;
+            i++;
+            if (i >= botones.Length)
+            {
+                i = 0;
+            }
+        }
+
+        int contadorChecks = 0;
+        private void checkBox_Checked(object sender, EventArgs e)
+        {
+            if (((CheckBox)sender).Checked)
+            {
+                toolTip.SetToolTip((CheckBox)sender, "Marcado");
+                contadorChecks++;
+            }
+            else
+            {
+                toolTip.SetToolTip((CheckBox)sender, "No marcado");
+                contadorChecks--;
+            }
+        }
+        private void FormPrincipal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (LstNombres.Items.Count > 0)
+            {
+                if (MessageBox.Show("Se saldrá de la aplicacion", this.Text, MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation) == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+            }
+            // Si la lista no tiene items sale directamente
+            else
+            {
+                e.Cancel = false;
+            }
+        }
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblResultados.Text = "Resultados";
+            foreach (Control c in Controls)
+            {
+                if (c is CheckBox)
+                {
+                    ((CheckBox)c).Checked = false;
+                    ((CheckBox)c).BackColor = Color.White;
+                    toolTip.SetToolTip((CheckBox)c, "No marcado");
+                }
+            }
+        }
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        int[] aleatorios;
+        private void btnJugar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
