896299c87bec17b87e5c358f58aa7b4c60a016d3
CojoCosta <diegocostapereira18@gmail.com>
2025-11-16 12:53:23 +0100

    changes 15/11

--- 1ªEvaluacion/RepasoExamen/RepasoExamen/FrmPrincipal.cs
+++ 1ªEvaluacion/RepasoExamen/RepasoExamen/FrmPrincipal.cs
@@ -12,6 +12,7 @@ namespace RepasoExamen
 {
     public partial class FrmPrincipal : Form
     {
+        CheckBox ch;
         public FrmPrincipal()
         {
             InitializeComponent();
@@ -19,7 +20,24 @@ namespace RepasoExamen
 
         private void FrmPrincipal_Load(object sender, EventArgs e)
         {
-
+            int x = 30;
+            int y = 30;
+            for (int i = 1; i < 50; i++)
+            {
+                ch = new CheckBox();
+                ch.Enabled = true;
+                ch.Visible = true;
+                ch.Size = new Size(30,30);
+                ch.Location = new Point(x,y);
+                ch.Text = i.ToString();
+                x += 40;
+                if (i % 9 == 0)
+                {
+                    x = 30;
+                    y += 40;
+                }
+                Controls.Add(ch);
+            }
         }
     }
 }
